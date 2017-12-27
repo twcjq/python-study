24779570b6e62f80561c6294bcfc32564a60f3ff
wingdi <l>
2017-12-27 10:35:31 +0800

    first commit

---
+++ python-4/Client.py
@@ -0,0 +1,10 @@
+from Student import Student
+
+if __name__ == "__main__":
+	xiaoMing = Student(1,"小明","三年级")
+	xiaoMing.goto_school()
+		
+
+
+
+		
---
+++ python-4/Student.py
@@ -0,0 +1,13 @@
+
+class Student():
+
+	def __init__(self,student_id,name,grade):
+		self.student_id = student_id
+		self.name = name
+		self.grade = grade
+
+	def goto_school(self):
+		print("去上学了！")
+
+
+		
