55cdb07a5d5f3eeafab0618b853f919fd57c1659
wingdi <lens_z@163.com>
2017-12-28 11:59:03 +0800

    update session-4

--- python-4/Student.py
+++ python-4/Student.py
@@ -1,13 +1,20 @@
-
+# -*- coding: UTF-8 -*-
+#定义一个学生类
 class Student():
 
-	def __init__(self,student_id,name,grade):
-		self.student_id = student_id
-		self.name = name
-		self.grade = grade
+    #初始化学生类
+    def __init__(self,student_id,name,grade):
+        self.student_id = student_id #为学生的学号赋值
+        self.name = name #为学生的名字赋值
+        self.grade = grade #为学生的年级赋值
 
-	def goto_school(self):
-		print("去上学了！")
+    #创建“去上学”这个行为    
+    def goto_school(self):
+        print(self.name+"去上学了!!!!!!!!!!") #代表去上学了
 
 
-		
+if __name__ == "__main__":
+    #创建xiaoMing这个对象
+    xiaoHong = Student(1,"小红","三年级")
+    #xiaoMing去上学
+    xiaoHong.goto_school()
